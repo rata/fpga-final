c4cfe33d1da18b888642524eb1499d8a1ab0d34c
Ignacio Mariano Tirabasso <itirabasso@ws21.labo7>
2014-10-22 12:49:58 -0300

    Esto anda.
    xx# Please enter the commit message for your changes. Lines starting

--- main.v
+++ main.v
@@ -31,10 +31,11 @@ wire [7:0] DI;
 wire [7:0] DO;
 
 assign write_enable = 1'b1;
-assign addr = 9'b0;
+assign addr = 10'b0;
 assign DI = 8'h3;
 
 meminferida mem(.clk(clk), .write_enable(write_enable), .addr(addr), .DI(DI), .DO(DO));
+
 
 wire [7:0] in_sseg_1;
 wire [7:0] in_sseg_2;
@@ -43,15 +44,14 @@ wire [7:0] in_sseg_4;
 wire [3:0] tmp;
 wire [3:0] tmp2;
 
-
-
 assign tmp = DO;
 assign tmp2 = DO >> 4;
 
 hex_to_sseg h2s_1 (.hex(tmp), .dp(1'b1), .sseg(in_sseg_1));
 hex_to_sseg h2s_2 (.hex(tmp2), .dp(1'b0), .sseg(in_sseg_2));
+
 //hex_to_sseg h2s_3 (.hex(tmp3), .dp(1'b0), .sseg(in_sseg_3));
 //hex_to_sseg h2s_4 (.hex(tmp4), .dp(1'b0), .sseg(in_sseg_4));
-disp_mux disp_m (.clk(clk), .reset(1'b0), .in0(in_sseg_2), .in1(in_sseg_1), .in2(8'hff), .in3(8'hff), .an(an), .sseg(sseg));
+disp_mux disp_m (.clk(clk), .reset(1'b0), .in0(in_sseg_1), .in1(in_sseg_2), .in2(8'hff), .in3(8'hff), .an(an), .sseg(sseg));
 
 endmodule
--- meminferida.v
+++ meminferida.v
@@ -21,9 +21,9 @@
 module meminferida(
 	input wire clk,
 	input wire write_enable,
-	input wire [9:0] addr,
-	input wire [7:0] DI,
-	output wire [7:0] DO	
+	input wire [RAM_ADDR_BITS-1:0] addr,
+	input wire [RAM_WIDTH-1:0] DI,
+	output wire [RAM_WIDTH-1:0] DO	
 ); 
 
 
@@ -32,8 +32,10 @@ parameter RAM_ADDR_BITS = 10;
 
 reg [RAM_WIDTH-1:0] image [(2**RAM_ADDR_BITS)-1:0];
 
-//initial
-//$readmemh("/home/itirabasso/fifo/imagentest.bin", image, 0, 7);
+/*
+initial
+$readmemh("/home/itirabasso/fifo/imagentest.bin", image, 0, 24);
+*/
 
 always @(posedge clk)
 	if (write_enable)
