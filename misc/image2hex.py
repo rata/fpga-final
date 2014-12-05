d851e18be9c3a91899f9a9f2725252d5cb5f5b79
Rodrigo Campos <rodrigo@sdfg.com.ar>
2014-12-05 10:31:05 -0300

    misc: Emprolijar los scripts de python

--- misc/image2hex.py
+++ misc/image2hex.py
@@ -14,21 +14,11 @@ fin  = Image.open(sys.argv[1])
 fout  = open(sys.argv[2], 'wb')
 
 pixels = fin.load()
-y_pxs = range(fin.size[1])
-#y_pxs.reverse()
-x_pxs = range(fin.size[0])
-#x_pxs.reverse()
 
-for y in y_pxs:
-    for x in x_pxs:
+for y in range(fin.size[1]):
+    for x in range(fin.size[0]):
 	buf1, buf2, buf3 = pixels[x, y]
-	#print "buf1", buf1
-	#print "buf2", buf2
-	#print "buf3", buf3
 	buf1_ascii = to_ascii_hex(buf1)
 	buf2_ascii = to_ascii_hex(buf2)
 	buf3_ascii = to_ascii_hex(buf3)
-	#print "buf1_ascii es:", buf1_ascii
-	#print "buf2_ascii es:", buf2_ascii
-	#print "buf3_ascii es:", buf3_ascii
 	fout.write(buf1_ascii + buf2_ascii + buf3_ascii + '\n')
--- misc/recv-image.py
+++ misc/recv-image.py
@@ -1,16 +1,14 @@
-
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import serial
 import sys
 import Image
-
 
 def recv_pixel(ser):
 	while True:
 		byte = ser.read(1)
 		if byte == '\0':
-			#print "skipping"
 			continue
-		#print "byte es", byte
 		return ord(byte)
 
 if len(sys.argv) != 4:
@@ -20,24 +18,14 @@ size = (int(sys.argv[1]), int(sys.argv[2]))
 dst_file = sys.argv[3]
 
 ser = serial.Serial("/dev/ttyUSB0", 19200, parity='N', rtscts=False, xonxoff=False)
-#byte = ser.read(1)
-#byte = ser.read(1)
 
-img = Image.new('RGB', size) # create a new black image
+img = Image.new('RGB', size)
 pixels = img.load()
 
-y_pxs = range(img.size[1])
-#y_pxs.reverse()
-x_pxs = range(img.size[0])
-#x_pxs.reverse()
-
-for y in y_pxs:
-    for x in x_pxs:
+for y in range(img.size[1]):
+    for x in range(img.size[0]):
 	px = recv_pixel(ser)
-	#print "receiving", px
-        pixels[x, y] = (px, px, px) # set the colour accordingly
+        pixels[x, y] = (px, px, px)
 
 img.save(dst_file)
-img.show()
-
 ser.close()
